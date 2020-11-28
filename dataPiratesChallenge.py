87f7008e34b010ddf0f33d72be8bba0dfc1d5f93
Luis Fernando de Oliveira <luis10.pf@gmail.com>
2020-11-27 23:37:10 -0300

    Lógica para pegar todas as paginas

--- dataPiratesChallenge.py
+++ dataPiratesChallenge.py
@@ -1,13 +1,30 @@
 import requests
 
-def reqFaixaCep(uf):
-  payload = {"UF": uf, "Localidade": "", "qtdrow": 112}
+qtdRow = 112 # nr maximo de rows por requisicao
+
+def reqFaixaCep(uf, pagini, pagfim):
+  payload = {"UF": uf, "qtdrow": qtdRow, "pagini": pagini, "pagfim": pagfim}
   r = requests.post("http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaFaixaCEP.cfm", data=payload)
   return r
 
-ufList = ['RS', 'SC']
+# ufList = ['RS', 'SC']
+ufList = ['RS']
 
 for uf in ufList:
-  value = reqFaixaCep(uf)
+  startRow = 1
+  endRow = 112
+  value = reqFaixaCep(uf, startRow, endRow)
+  # logica para inserir os valores em uma list abaixo e pegar o maxrow do html
   print(value.status_code)
+
+  maxRow = 534 #aqui vou atualizar o valor do maxRow pegando de dentro do html
+
+
+  while endRow <= maxRow:
+    startRow = endRow + 1
+    endRow += qtdRow
+    value = reqFaixaCep(uf, startRow, endRow)
+    # logica para inserir os valores em uma list abaixo
+    print(value.status_code)
+
 
