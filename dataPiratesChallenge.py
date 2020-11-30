b4ed09b815eee2bee8482bd55e43df54c4dc19ff
Luis Fernando de Oliveira <luis10.pf@gmail.com>
2020-11-29 22:54:58 -0300

    final

--- dataPiratesChallenge.py
+++ dataPiratesChallenge.py
@@ -1,63 +1,43 @@
 import requests
+import json
 import re
 from bs4 import BeautifulSoup
 
-qtdRow = 112 # nr maximo de rows por requisicao
-ufList = ['RS'] # UFs que serao buscadas
+qtdRow = 110 # nr maximo de rows por requisicao
+ufList = ['RS','SC'] # UFs que serao buscadas
 data = {} # resultado final
 
-# data = {
-#     'RS': [
-#       {'id':0, 'faixaCep':'tal a tal', 'localidade':'passo fundo'},
-#       {'id':0, 'faixaCep':'tal a tal', 'localidade':'passo fundo'},
-#       {'id':0, 'faixaCep':'tal a tal', 'localidade':'passo fundo'},
-#       {'id':0, 'faixaCep':'tal a tal', 'localidade':'passo fundo'}
-#     ],
-#     'SC': [
-#       {'id':0, 'faixaCep':'tal a tal', 'localidade':'passo fundo'},
-#       {'id':0, 'faixaCep':'tal a tal', 'localidade':'passo fundo'},
-#       {'id':0, 'faixaCep':'tal a tal', 'localidade':'passo fundo'},
-#       {'id':0, 'faixaCep':'tal a tal', 'localidade':'passo fundo'}
-#     ]
-# }
-# 
-
 def reqFaixaCep(uf, pagini, pagfim):
-  payload = {'UF': uf, 'Localidade':'Passo Fundo','qtdrow': qtdRow, 'pagini': pagini, 'pagfim': pagfim}
-  # payload = {'UF': uf, 'qtdrow': qtdRow, 'pagini': pagini, 'pagfim': pagfim}
+  payload = {'UF': uf, 'qtdrow': qtdRow, 'pagini': pagini, 'pagfim': pagfim}
   try:
-    r = requests.post('http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaFaixaCEP.cfm', data=payload)
+    r = requests.post('http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaFaixaCEP.cfm', data=payload, allow_redirects=False)
   except requests.exceptions.RequestException as e:
     print('An error has occurred. Please try again later.')
     raise SystemExit(e)
 
   return r
 
-def findMaxRow(html):
-  findMaxRow = html.find(text=re.compile('1 a 112 de '))
-  if(findMaxRow):
+def findMaxRow(uf, startRow, endRow):
+  page = reqFaixaCep(uf, startRow, endRow)
+  soup = BeautifulSoup(page.text, 'html.parser')
+  findMaxRow = soup.find(text=re.compile('1 a 110 de '))
+  if (findMaxRow):
     words = findMaxRow.strip().split()
     return words[-1]
   else:
     return 0
 
-for uf in ufList:
-  startRow = 1
-  endRow = 112
-  page = reqFaixaCep(uf, startRow, endRow)
-
-  # logica para inserir os valores em uma list abaixo e pegar o maxrow do html
+def getCepList(page):
+  global id
   soup = BeautifulSoup(page.text, 'html.parser')
   content = soup.find('div', class_='ctrlcontent')
 
-  maxRow = findMaxRow(content)
+  tableRows = content.find_all('tr')
 
-  tableRows = content.find_all('tr', attrs={'bgcolor':'#C4DEE9'})
-
-  cepList = []
   for tableRow in tableRows:
     cells = tableRow.findAll('td')
     info = {}
+    info['id'] = id
 
     aux = 0
     for cell in cells:
@@ -72,18 +52,36 @@ for uf in ufList:
       else:
         info[aux] = cell.text
       aux += 1
-    cepList.append(info)
-  
+    
+    if (len(info) > 3):
+      cepList.append(info)
+      id += 1
+    else:
+      pass
+
+for uf in ufList:
+  id = 0
+  startRow = 1
+  endRow = 110
+  maxRow = int(findMaxRow(uf, startRow, endRow))
+  cepList = []
+  page = reqFaixaCep(uf, startRow, endRow)
+
+  # logica para inserir os valores em uma list abaixo e pegar o maxrow do html
+  getCepList(page)
+
+  while endRow <= maxRow:
+    startRow = endRow + 1
+    endRow += qtdRow
+    page = reqFaixaCep(uf, startRow, endRow)
+
+    # logica para inserir os valores em uma list abaixo
+    getCepList(page)
+
   data.update({uf: cepList})
 
-  # while endRow <= maxRow:
-  #   startRow = endRow + 1
-  #   endRow += qtdRow
-  #   page = reqFaixaCep(uf, startRow, endRow)
-  #   # logica para inserir os valores em uma list abaixo
-  #   print(page.status_code)
-
-
-print(data)
+with open('faixas_de_cep.json', 'w') as fp:
+  json.dump(data, fp)
+# print(data)
 
 
